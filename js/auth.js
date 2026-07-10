d64c8de1a963b9a30ba9c549f074c8936f091dec
codewithahmed2005 <codewithahmed2005@gmail.com>
2026-07-10 14:57:25 -0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'https://codewithahmed-scaneats-backend.hf.space';
 
 document.addEventListener('DOMContentLoaded', async () => {
     // Agar user already login hai toh dashboard par bhej do
